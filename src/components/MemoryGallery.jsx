9407d6825e59dd4952e3872c9763927f10b7462e
Syafizam <crawlernight548@gmail.com>
2026-04-08 22:28:56 +0800

    apa sial

--- src/components/MemoryGallery.jsx
+++ src/components/MemoryGallery.jsx
@@ -6,20 +6,20 @@ import './MemoryGallery.css';
 // Centralized image list
 const images = [
   // PUBG Memories
-  { src: './public/PUBG1.jpeg' }, { src: './public/PUBG2.jpeg' }, { src: './public/PUBG3.jpeg' },
-  { src: './public/PUBG4.jpeg' }, { src: './public/PUBG5.jpeg' }, { src: './public/PUBG6.jpeg' },
-  { src: './public/PUBG7.jpeg' }, { src: './public/PUBG8.jpeg' }, { src: './public/PUBG9.jpeg' },
+  { src: 'public/PUBG1.jpeg' }, { src: 'public/PUBG2.jpeg' }, { src: 'public/PUBG3.jpeg' },
+  { src: 'public/PUBG4.jpeg' }, { src: 'public/PUBG5.jpeg' }, { src: 'public/PUBG6.jpeg' },
+  { src: 'public/PUBG7.jpeg' }, { src: 'public/PUBG8.jpeg' }, { src: 'public/PUBG9.jpeg' },
   // Fizam & Wana
-  { src: './public/Fizam.jpeg' }, { src: './public/Fizam2.jpg' }, { src: './public/Fizam3.jpg' },
-  { src: './public/Fizam4.jpg' }, { src: './public/Fizam5.jpg' }, { src: './public/Fizam6.jpg' },
-  { src: './public/Fizam7.jpg' }, { src: './public/Fizam8.jpg' }, { src: './public/Fizam9.jpg' },
-  { src: './public/Fizam10.jpg' }, { src: './public/Fizam11.png' },
+  { src: 'public/Fizam.jpeg' }, { src: 'public/Fizam2.jpg' }, { src: 'public/Fizam3.jpg' },
+  { src: 'public/Fizam4.jpg' }, { src: 'public/Fizam5.jpg' }, { src: 'public/Fizam6.jpg' },
+  { src: 'public/Fizam7.jpg' }, { src: 'public/Fizam8.jpg' }, { src: 'public/Fizam9.jpg' },
+  { src: 'public/Fizam10.jpg' }, { src: 'public/Fizam11.png' },
   // Wana Special
-  { src: './public/Wana2.jpeg' }, { src: './public/Wana3.jpeg' }, { src: './public/Wana4.jpeg' },
-  { src: './public/Wana5.jpeg' }, { src: './public/Wana6.jpeg' }, { src: './public/Wana7.jpeg' },
-  { src: './public/Wana8.jpeg' }, { src: './public/Wana9.jpeg' }, { src: './public/Wana10.jpeg' },
-  { src: './public/Wana11.jpeg' }, { src: './public/Wana12.jpeg' }, { src: './public/Wana13.jpeg' },
-  { src: './public/Wana14.jpeg' }, { src: './public/Wana15.jpeg' },
+  { src: ' public/Wana2.jpeg' }, { src: 'public/Wana3.jpeg' }, { src: 'public/Wana4.jpeg' },
+  { src: 'public/Wana5.jpeg' }, { src: 'public/Wana6.jpeg' }, { src: 'public/Wana7.jpeg' },
+  { src: 'public/Wana8.jpeg' }, { src: 'public/Wana9.jpeg' }, { src: 'public/Wana10.jpeg' },
+  { src: 'public/Wana11.jpeg' }, { src: 'public/Wana12.jpeg' }, { src: 'public/Wana13.jpeg' },
+  { src: 'public/Wana14.jpeg' }, { src: 'public/Wana15.jpeg' },
 ];
 
 const MemoryGallery = () => {
